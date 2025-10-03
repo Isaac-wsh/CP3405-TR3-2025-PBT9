e7a8f1c040a11ed2892d7e17cdcc113766c0307e
Isaac <shiheng.wang@my.jcu.edu.au>
2025-10-03 19:57:00 +0800

    Test Branch

---
+++ hello_team.py
@@ -0,0 +1,21 @@
+"""
+SmartSeat - Test Branch Commit
+This is a test file used to verify our branch development and Pull Request process.
+"""
+
+def team_greeting():
+    team_members = ["Wang Shiheng - Design IC",
+        "Liangzhe Cai - Engineer IC",
+        "Wu Buqing - Researcher",
+        "Chen Zhuangzhuang - UX Writer",
+        "Shijie Shen - Data Scientist",
+        "Shaoheng Song - Product Marketing Manager",
+        "Bo Yuan - Product Owner"]
+    print("Hello from SmartSeat team!")
+    print("Team members are:")
+    for member in team_members:
+        print(f"- {member}")
+
+
+if __name__ == "__main__":
+    team_greeting()
